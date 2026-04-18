71dbe021159b4b4c852c2fcad316624c0b462cf0
leo <leonardo.groome@gmail.com>
2026-04-17 22:19:49 -0600

    chore: remove unused Remotion package and associated brainstorm artifacts

--- packages/remotion/remotion.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from '@remotion/cli/config'
-
-Config.setVideoImageFormat('jpeg')
-Config.setCodec('h264')
-Config.setWidth(1920)
-Config.setHeight(1080)
-Config.setFps(30)
-Config.setPixelFormat('yuv420p')
-// Overridable per-composition via CLI flags
